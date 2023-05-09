eed0d9dc89edcf2bac25ce06f92ad6ac0c112fd4
Ghost <danyfox1.dm@gmail.com>
2023-05-08 21:07:48 -0300

    gitignore

--- game.rb
+++ game.rb
@@ -1,14 +1,3 @@
-require 'colorize'
-
-arr_words = []
-
-words = File.open('./google-10000-english-no-swears.txt', 'r') do |word|
-  all = word.readlines
-  all.select do |big|
-    if big.size.between?(6, 13)
-      arr_words << big
-    end
-  end
+class Game 
+  
 end
-
-puts arr_words.shuffle.sample
