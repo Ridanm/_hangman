967c9798459b4845d8d44c859713880a8db658a9
Ghost <danyfox1.dm@gmail.com>
2023-05-10 21:33:37 -0300

    adding text

--- presentation_module.rb
+++ presentation_module.rb
@@ -4,9 +4,12 @@ module Presentation
   def self.show_phrase(string)
     {
       'title' => 'Welcome to the Hangman game',
-      'guess' => 'Guess the word each underscore represents a letter',
-      'turns' => 'It depend on the word are the turns you have to guess',
+      'guess' => 'Guess the word each underscore represents a letter.',
+      'only_first' => 'If you enter more than one letter, only the first will be valid.',
+      'turns' => 'It depend on the word are the turns you have to guess, in this case: ',
       'save' => 'If you want to save the game before exiting type: save',
+      'enter_char' => 'Enter a char from a to z: ',
+      'warning' => 'Enter only characters from a to z',
       'winner' => 'Congratulations you guessed the word',
       'loose' => "The word hasn't been guessing do you want try again type: yes",
       'exit' => 'If you have leave the game in any time write: exit',
