667f405e7e3d83dcab4ea5b3e49b66a4799744fd
Ghost <danyfox1.dm@gmail.com>
2023-05-23 20:47:25 -0300

    grouping files

--- main.rb
+++ main.rb
@@ -1,4 +1,10 @@
 require './secret_word.rb'
+require './presentation_module'
+require './save_module.rb'
+require './player.rb'
+require './game.rb'
 
-word = SecretWord.new 
-puts word.select_word 
+word = SecretWord.new.select_word 
+player = Player.new
+game = Game.new(word, player)
+game.beginning 
